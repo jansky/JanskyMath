c98fdd6883f8e37be126142e6f27ddec208c14b2
jansky <jansky@users.noreply.github.com>
2014-04-04 12:22:02 -0400

    Update Geometry.cs

--- Geometry.cs
+++ Geometry.cs
@@ -13,18 +13,69 @@ namespace JanskyMath
       y = yarg;
     }
     
-    public class Line
+    //String format should be (x,y)
+    public Point(string pointAsString)
     {
-      public List<Point> Points = new List<Point>();
+      x = Convert.ToDouble(pointAsString[1]);
+      y = Convert.ToDouble(pointAsString[3]);
+    }
+    
+    public string ToString()
+    {
+      return "(" + Convert.ToString(x) + "," + Convert.ToString(y) + ")";
+    }
+    
+    
+  }
+    
+
+    
+    public class Segment
+    {
+      public Point StartPoint;
+      public Point EndPoint;
       public Fraction Slope;
       
-      public Line(Point p1, Point p2)
+      public Segment (Point sp, Point ep)
       {
-        Points.Add(p1);
-        Points.Add(p2);
+        StartPoint = sp;
+        EndPoint = ep;
         
         //Calculate the slope
-        Slope = new Fraction(p2.y - p1.y, p2.x - p1.x);
+        Slope = new Fraction(ep.y - sp.y, ep.x - sp.x);
+      }
+      
+      public Segment (Point sp, Point ep, Fraction segmentSlope)
+      {
+        StartPoint = sp;
+        EndPoint = ep;
+        
+        Slope = segmentSlope;
+      }
+    }
+    
+    public class Operations
+    {
+      public static Fraction CalculateSlope(Point p1, Point p2)
+      {
+        return new Fraction(p2.y - p1.y, p2.x - p1.x);
+      }
+      
+      public static Point CalculateMidpoint(Segment s)
+      {
+        double x1 = s.StartPoint.x;
+        double x2 = s.EndPoint.x;
+        
+        double y1 = s.StartPoint.y;
+        double y2 = s.EndPoint.y;
+        
+        
+        double xMean = (x1 + x2) / 2;
+        double yMean = (y1 + y2) / 2;
+        
+        return new Point(xMean, yMean);
+        
+        
       }
     }
   }
