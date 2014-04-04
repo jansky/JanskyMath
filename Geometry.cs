520276dabe8284e52fdd2fc84d1fead419d697e7
jansky <jansky@users.noreply.github.com>
2014-04-04 12:04:56 -0400

    Update Geometry.cs

--- Geometry.cs
+++ Geometry.cs
@@ -1,1 +1,30 @@
-namespace 
+namespace System;
+
+namespace JanskyMath
+{
+  public class Point
+  {
+    public double x {get; set;}
+    public double y {get; set;}
+    
+    public Point(double xarg, double yarg)
+    {
+      x = xarg;
+      y = yarg;
+    }
+    
+    public class Line
+    {
+      public List<Point> Points = new List<Point>();
+      public Fraction Slope;
+      
+      public Line(Point p1, Point p2)
+      {
+        Points.Add(p1);
+        Points.Add(p2);
+        
+        //Calculate the slope
+        Slope = new Fraction(p2.y - p1.y, p2.x - p1.x);
+      }
+    }
+  }
